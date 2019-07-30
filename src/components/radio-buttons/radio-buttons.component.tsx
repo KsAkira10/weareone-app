748857e5db683946fa5b1d97f7ff54afa9768c0f
Lucas Akira Uehara <akira@MacBook-Pro-de-Lucas.local>
2019-07-30 08:46:15 -0300

    feat(radio-button): add responsive buttons

--- src/components/radio-buttons/radio-buttons.component.tsx
+++ src/components/radio-buttons/radio-buttons.component.tsx
@@ -5,15 +5,40 @@ export interface IRadioButtons {
   handleClick: any;
 }
 
+const getRadioButtons = () => {
+  return document.querySelectorAll('.radio-button');
+};
+
+const RadioButtonActive = (element: Element) => {
+  getRadioButtons().forEach((button) => {
+    if (button.classList.contains('radio-button--active')) {
+      button.classList.remove('radio-button--active');
+    }
+  });
+
+  if (element.classList.contains('radio-button--active')) {
+    element.classList.remove('radio-button--active');
+  } else {
+    element.classList.add('radio-button--active');
+  }
+};
+
 const RadioButtons: React.FC<IRadioButtons> = ({ handleClick }) => (
   <div className="radio-buttons">
-    <button type="button" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--technobase" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--hardbase" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--trancebase" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--coretime" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--teatime" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--clubtime" onClick={handleClick}></button>
+    <button type="button" className="radio-button radio-button--housetime  radio-button--active" onClick={handleClick}></button>
   </div>
 );
 
 RadioButtons.defaultProps = {
   handleClick: (e: React.MouseEvent) => {
-    console.log(e.clientX);
+    e.preventDefault();
+    RadioButtonActive(e.currentTarget);
   }
 };
 
